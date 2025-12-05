88895171c1dd96c08821101cf2a5ef7233276561
cleonavila <cleon@almagrario.com>
2025-12-05 16:42:45 -0500

    Ajustes

--- src/app/dashboard/pages/product-list-admin/product-list-admin.component.ts
+++ src/app/dashboard/pages/product-list-admin/product-list-admin.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductTableComponent } from "@products/components/product-table/product-table.component";
+import { ProductsService } from '@products/services/products.service';
+import { PaginationService } from '@share/pagination/pagination.service';
+import { PaginationComponent } from "@share/pagination/pagination.component";
 
 @Component({
   selector: 'app-product-list-admin',
-  imports: [ProductTableComponent],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './product-list-admin.component.html',
   styles: ``
 })
 export class ProductListAdminComponent {
 
+  private productService = inject(ProductsService);
+  pagination = inject(PaginationService);
+
+  productsResource = rxResource({
+    request: () => ({page: this.pagination.currentPage() - 1}),
+    loader: ({ request }) => {
+      console.log(request);
+
+      return this.productService.getProducts({
+        offset: request.page * 9
+      });
+    }
+  });
 }
